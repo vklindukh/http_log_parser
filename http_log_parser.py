bf8d3ea10f8585c222db9044baa387afca769476
Valentyn Klindukh <vklindukh@cogniance.com>
2017-07-27 10:38:33 +0300

    adding comments

--- http_log_parser.py
+++ http_log_parser.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python
+#
+# Usage: To get command-line options run http_log_parser.py -h
+#
 
 import argparse
 import re
 
+# list of statistics calculated and displayed by default.
 STATS_FULL_LIST = "top10,success,unsuccess,top10unsuccess,top10ips,timestat"
 
 
+#
+# parse command-line options
+#
 class Options(object):
     def __init__(self, stats_list):
         self.stats_list = stats_list
 
         parser = argparse.ArgumentParser()
         parser.add_argument("-f", "--file", dest="file",
-                          help="apache access log to process")
+                            help="apache access log to process")
         parser.add_argument("-s", "--statistics", dest="statistics", default=self.stats_list,
-                          help="list of coma separated statistics to be calculated. Default is %s" % self.stats_list)
+                            help="list of coma separated statistics to be calculated. Default is %s" % self.stats_list)
         parser.add_argument("-q", action="store_false", dest="strip", default=True,
-                          help="include query string into URL. Strip by default")
+                            help="include query string into URL. Strip by default")
 
         self.args = parser.parse_args()
         if not self.args.file:
             parser.error("file name is required")
 
+    #
+    # method returns dictionary, where the key is name of statistic (see STATS_FULL_LIST)
+    # statistic would be calculated if the value is True
+    #
     def get_stat_list(self):
         slist = dict(map(lambda x: [x, False], self.stats_list.split(",")))
         for s in self.args.statistics.split(","):
@@ -35,9 +46,13 @@ class Options(object):
         return self.args.strip
 
 
+#
+# apache-log parser. extracts metrics from log file, calculates statistics, display results
+#
 class Parser(object):
     def __init__(self, stats_list, strip):
         self.strip = strip
+        # stats_list varable contains
         self.stats_list = stats_list
         self.counter = dict()
         self.counter['total'] = 0
@@ -51,8 +66,15 @@ class Parser(object):
 
         self.matcher = re.compile(r"^([0-9.]+)\s+-\s+-\s+\[(\S+)\s+\S+\]\s+\"\S+\s+(\S+)\s+\S+\"\s+(\d+)\s+\d+$")
 
+    #
+    # parser method. process single line, extract metrics and calculates statistics requested
+    #
     def process(self, l):
         g = self.matcher.match(l.strip())
+        if g is None:
+            print "unable to parse string {}".format(l)
+            return
+
         url = g.group(3)
         if self.strip:
             url = url.split("?")[0]
@@ -76,37 +98,61 @@ class Parser(object):
         if self.stats_list['timestat']:
             self.process_list('timestat', data_strip)
 
-    def process_list(self, param, value):
-        if value not in self.counter[param]:
-            self.counter[param][value] = 1
+    #
+    # internal method. It gets param/key pair and updates the nested dictionary:
+    #   - creates new element, if not exist
+    #   - increases counter
+    #
+    def process_list(self, param, key):
+        if key not in self.counter[param]:
+            self.counter[param][key] = 1
         else:
-            self.counter[param][value] += 1
+            self.counter[param][key] += 1
 
-    def process_dict(self, param, value, value2):
-        if value not in self.counter[param]:
-            self.counter[param][value] = dict()
+    #
+    # internal method. It gets param and key/value pair and updates the nested dictionaries:
+    #   - creates new element (empty nested dictionary) if not exist
+    #   - inside nested dictionary:
+    #      - creates new element, if not exist
+    #      - increases counter
+    #
+    def process_dict(self, param, key, key2):
+        if key not in self.counter[param]:
+            self.counter[param][key] = dict()
 
-        if value2 not in self.counter[param][value]:
-            self.counter[param][value][value2] = 1
+        if key2 not in self.counter[param][key]:
+            self.counter[param][key][key2] = 1
         else:
-            self.counter[param][value][value2] += 1
+            self.counter[param][key][key2] += 1
 
+    #
+    # internal method. calls self.process_list() (i.e. updates counters) only for unsuccessful requests
+    #
     def process_list_unsuccess(self, url, code):
         if not self.success(code):
             self.process_list('urlunsuccess', url)
 
+    #
+    # internal method. updated 'success' on 'unsuccess' counters depending on HTTP response code
+    #
     def process_total(self, code):
         if self.success(code):
             self.counter['success'] += 1
         else:
             self.counter['unsuccess'] += 1
 
+    #
+    # internal method. Check HTTP response code and returns 'True' forr successful and 'Flase' for unsuccessful requests
+    #
     def success(self, code):
         if code.startswith('2') or code.startswith('3'):
             return True
         else:
             return False
 
+    #
+    # print requested statistics to STDOUT
+    #
     def print_stat(self):
 
         if self.stats_list['timestat']:
